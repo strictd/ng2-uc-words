e8b6a1b3fb72521f592c8e55268fc0a635b00bf2
Gary Taylor <gravity.addiction@gmail.com>
2016-05-10 08:46:10 -0700

    init

---
+++ uc-words.js
@@ -0,0 +1,29 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var UcWords = (function () {
+    function UcWords() {
+    }
+    UcWords.prototype.transform = function (str) {
+        return (str + '').replace(/^([a-z])|\s+([a-z])/g, function ($1) {
+            return $1.toUpperCase();
+        });
+    };
+    UcWords = __decorate([
+        core_1.Pipe({
+            name: 'UcWords'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], UcWords);
+    return UcWords;
+}());
+exports.UcWords = UcWords;
+//# sourceMappingURL=uc-words.js.map
